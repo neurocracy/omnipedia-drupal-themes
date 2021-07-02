23f62d0a263a49d440ec57be759fe6be471faca0
Matei Stanca <i@ambientimpact.com>
2021-07-01 21:09:23 -0400

    Header search overlay now prevents viewport scrolling when open.

--- omnipedia_site/javascript/header.overlay.js
+++ omnipedia_site/javascript/header.overlay.js
@@ -4,12 +4,15 @@
 
 // This adds a one off click handler to the <main> element (which the overlay is
 // generated content of) when the search field is active, allowing a click or
-// tap on the overlay to close the search field and overlay.
+// tap on the overlay to close the search field and overlay. Additionally, this
+// marks that same element as being an active overlay for the purpose of
+// preventing viewport scrolling while the overlay is open.
 
 AmbientImpact.on([
   'OmnipediaSiteThemeHeaderElements',
   'OmnipediaSiteThemeHeaderState',
-], function(headerElements, headerState, $) {
+  'OmnipediaSiteThemeOverlayScroll',
+], function(headerElements, headerState, overlayScroll, $) {
 AmbientImpact.addComponent('OmnipediaSiteThemeHeaderOverlay', function(
   headerOverlay, $
 ) {
@@ -32,6 +35,8 @@ AmbientImpact.addComponent('OmnipediaSiteThemeHeaderOverlay', function(
         'omnipediaSearchActive.OmnipediaSiteThemeHeaderOverlay',
       function(event) {
 
+        overlayScroll.overlayOpened($main);
+
         $main.one('click.OmnipediaSiteThemeHeaderOverlay', function(event) {
           headerState.hideSearch();
         });
@@ -42,12 +47,21 @@ AmbientImpact.addComponent('OmnipediaSiteThemeHeaderOverlay', function(
 
         $main.off('click.OmnipediaSiteThemeHeaderOverlay');
 
+        overlayScroll.overlayClosed($main);
+
       });
 
     },
     function(context, settings, trigger) {
 
-      $('main[role="main"]', context).off(
+      /**
+       * The site content <main> element, wrapped in a jQuery collection.
+       *
+       * @type {jQuery}
+       */
+      var $main = $('main[role="main"]', context);
+
+      $main.off(
         'click.OmnipediaSiteThemeHeaderOverlay'
       );
 
@@ -55,6 +69,10 @@ AmbientImpact.addComponent('OmnipediaSiteThemeHeaderOverlay', function(
         'omnipediaSearchActive.OmnipediaSiteThemeHeaderOverlay',
         'omnipediaSearchInactive.OmnipediaSiteThemeHeaderOverlay',
       ].join(' '));
+
+      // Make sure to mark the overlay as closed in case it was opened on
+      // detach.
+      overlayScroll.overlayClosed($main);
 
     }
   );
