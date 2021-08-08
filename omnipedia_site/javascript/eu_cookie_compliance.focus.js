5b8f07f923b221ca52058ebc2bfad2532a9fa09b
Matei Stanca <i@ambientimpact.com>
2021-08-08 15:06:15 -0400

    Cookie pop-up now gains focus on open, returns focus to toggle on close.

---
+++ omnipedia_site/javascript/eu_cookie_compliance.focus.js
@@ -0,0 +1,100 @@
+// -----------------------------------------------------------------------------
+//   Omnipedia - Site theme - EU cookie compliance focus management
+// -----------------------------------------------------------------------------
+
+// This manages focus on the following events:
+//
+// - 'euCookieCompliancePopUpOpened': moves focus to the first tabbable element
+//   in the pop-up.
+//
+// - 'euCookieCompliancePopUpClose': moves focus to the privacy settings toggle.
+
+AmbientImpact.onGlobals('ally.query.tabbable', function() {
+AmbientImpact.on([
+  'pointerFocusHide',
+  'OmnipediaPrivacySettings',
+  'OmnipediaSiteThemeEuCookieComplianceElements',
+  'OmnipediaSiteThemeEuCookieComplianceState',
+], function(
+  aiPointerFocusHide,
+  OmnipediaPrivacySettings,
+  euCookieComplianceElements,
+  euCookieComplianceState
+) {
+AmbientImpact.addComponent(
+  'OmnipediaSiteThemeEuCookieComplianceFocus',
+function(
+  euCookieComplianceFocus, $
+) {
+
+  'use strict';
+
+  this.addBehaviour(
+    'OmnipediaSiteThemeEuCookieComplianceFocus',
+    'omnipedia-site-theme-eu-cookie-compliance-focus',
+    euCookieComplianceElements.getBehaviourSelector(),
+    function(context, settings) {
+
+      /**
+       * The cookie compliance pop-up, if any, wrapped in a jQuery collection.
+       *
+       * @type {jQuery}
+       */
+      var $popUp = euCookieComplianceElements.getPopUp();
+
+      // Bail if we can't find the pop-up.
+      if ($popUp.length === 0) {
+        return;
+      }
+
+      $popUp.attr('tab-index', '-1');
+
+      $popUp
+      .on(
+        'euCookieCompliancePopUpOpened.OmnipediaSiteThemeEuCookieComplianceFocus',
+      function(event) {
+
+        aiPointerFocusHide.lock();
+
+        // We have to find the first tabbable element in the pop-up because the
+        // pop-up itself does not seem to be focusable for some reason.
+        $(ally.query.tabbable({
+          context:        $popUp,
+          includeContext: true
+        })).first().focus();
+
+        aiPointerFocusHide.unlock();
+
+     })
+      .on(
+        'euCookieCompliancePopUpClose.OmnipediaSiteThemeEuCookieComplianceFocus',
+      function(event) {
+        OmnipediaPrivacySettings.getToggle().focus();
+      });
+
+    },
+    function(context, settings, trigger) {
+
+      /**
+       * The cookie compliance pop-up, if any, wrapped in a jQuery collection.
+       *
+       * @type {jQuery}
+       */
+      var $popUp = euCookieComplianceElements.getPopUp();
+
+      // Bail if we can't find the pop-up.
+      if ($popUp.length === 0) {
+        return;
+      }
+
+      $popUp.off([
+        'euCookieCompliancePopUpOpen.OmnipediaSiteThemeEuCookieComplianceFocus',
+        'euCookieCompliancePopUpClose.OmnipediaSiteThemeEuCookieComplianceFocus',
+      ].join(' '));
+
+    }
+  );
+
+});
+});
+});
