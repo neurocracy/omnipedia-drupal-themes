3c1cb8f3e646252eb7b7502d9aa2ea555e73235d
Matei Stanca <i@ambientimpact.com>
2021-08-10 14:06:38 -0400

    eu_cookie_compliance.focus.js: toggle only focus on non-pointer source.

--- omnipedia_site/javascript/eu_cookie_compliance.focus.js
+++ omnipedia_site/javascript/eu_cookie_compliance.focus.js
@@ -10,17 +10,22 @@
 // - 'euCookieCompliancePopUpClosed': moves focus to the privacy settings
 //   toggle.
 
-AmbientImpact.onGlobals('ally.query.tabbable', function() {
+AmbientImpact.onGlobals([
+  'ally.query.tabbable',
+  'ally.style.focusSource',
+], function() {
 AmbientImpact.on([
   'pointerFocusHide',
   'OmnipediaPrivacySettings',
   'OmnipediaSiteThemeEuCookieComplianceElements',
   'OmnipediaSiteThemeEuCookieComplianceState',
+  'OmnipediaSiteThemeSidebarsState',
 ], function(
   aiPointerFocusHide,
   OmnipediaPrivacySettings,
   euCookieComplianceElements,
-  euCookieComplianceState
+  euCookieComplianceState,
+  sidebarsState
 ) {
 AmbientImpact.addComponent(
   'OmnipediaSiteThemeEuCookieComplianceFocus',
@@ -29,6 +34,19 @@ function(
 ) {
 
   'use strict';
+
+  /**
+   * The ally.js focus source global service object.
+   *
+   * This initializes ally.js' focus source service, which starts watching the
+   * document and applies the data-focus-source attribute to the <html> element.
+   *
+   * @type {Object}
+   *
+   * @see https://allyjs.io/api/style/focus-source.html
+   *   ally.js documentation.
+   */
+  var focusSourceHandle = ally.style.focusSource();
 
   this.addBehaviour(
     'OmnipediaSiteThemeEuCookieComplianceFocus',
@@ -76,7 +94,25 @@ function(
         'euCookieCompliancePopUpClosed.OmnipediaSiteThemeEuCookieComplianceFocus',
       function(event) {
 
-        OmnipediaPrivacySettings.getToggle().focus();
+        // Only focus the toggle if the focus source was not the pointer, or if
+        // sidebars are not off-canvas. This is to prevent the sidebar menu
+        // unexpectedly opening if the pop-up was open due to some other reason,
+        // and not because it was invoked via the toggle in the sidebar. The
+        // most likely reason for this would be when the user has not yet agreed
+        // to anything in the pop-up, either on first navigating to the site or
+        // because they clicked "Decide later" on a previous page.
+        //
+        // @todo We need a better way of handling this that allows focus to be
+        //   moved into the sidebar for the purpose of maintaining tabbing order
+        //   but without causing the sidebar menu to open. One way to do this
+        //   could be to alter the CSS to only show on :focus-within if
+        //   html:not([data-focus-source="pointer"])
+        if (
+          focusSourceHandle.current() !== 'pointer' ||
+          !sidebarsState.isOffCanvas()
+        ) {
+          OmnipediaPrivacySettings.getToggle().focus();
+        }
 
         aiPointerFocusHide.unlock();
 
