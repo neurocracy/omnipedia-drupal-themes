e04fc20582c78bbe4e34736f82c304fa4da36a10
Matei Stanca <i@ambientimpact.com>
2022-07-11 20:36:29 -0400

    sidebars.sticky.js: work-in-progress conversion to instantiable object.

--- javascript/sidebars.sticky.js
+++ javascript/sidebars.sticky.js
@@ -24,11 +24,11 @@ function(sidebarsSticky, $) {
   'use strict';
 
   /**
-   * Class added to the sidebars container when JavaScript sticky is in use.
+   * Class added to the layout container when JavaScript sticky is in use.
    *
    * @type {String}
    */
-  const containerEnhancedStickyClass = 'layout-sidebars--is-enhanced-sticky';
+  const containerEnhancedStickyClass = 'layout-container--is-enhanced-sticky';
 
   /**
    * Event namespace name.
@@ -87,6 +87,124 @@ function(sidebarsSticky, $) {
    */
   const topOffsetPropertyName = '--sticky-sidebar-top-offset';
 
+  const stickyElementInstancePropertyName = 'stickyElementInstance';
+
+  /**
+   * Active sticky element instances
+   *
+   * @type {Object}
+   *
+   * @todo Convert this to a reusable instance manager that can trigger an event
+   *   (so we know when to remove the .layout-container class for example) when
+   *   all instances or values have been removed/destroyed.
+   */
+  let activeInstances = {};
+
+  function stickyElement($element, stickySettings) {
+
+    let thisInstance = this;
+
+    /**
+     * Sticky Sidebar instance.
+     *
+     * @type {StickySidebar}
+     */
+    let stickySidebarsInstance;
+
+    /**
+     * Property to pixel converter instance.
+     *
+     * @type {converter}
+     */
+    let propertyConverterInstance = aiPropertyToPixelConverter.create(
+      $element, [bottomOffsetPropertyName, topOffsetPropertyName]
+    );
+
+    /**
+     * A responsive style property instance; watches sticky state.
+     *
+     * @type {responsiveStyleProperty}
+     */
+    let responsiveStyleProperty = aiResponsiveStyleProperty.create(
+      isStickyPropertyName, $element
+    );
+
+    function responsivePropertyHandler(event, instance) {
+
+      // Ignore any other responsive properties that may be watched on this
+      // or descendent elements.
+      if (instance.getPropertyName() !== isStickyPropertyName) {
+        return;
+      }
+
+      // Attempt to build an instance if one is not already active.
+      if (instance.getValue() === 'true') {
+        thisInstance.enable();
+
+      // Attempt to destroy an instance if one is active.
+      } else if (instance.getValue() === 'false') {
+        thisInstance.disable();
+      }
+
+    };
+
+    $element.on(
+      'responsivePropertyChange.' + eventNamespace, responsivePropertyHandler
+    );
+
+    this.enable = function() {
+
+      if (typeof stickySidebarsInstance !== 'undefined') {
+        return Promise.resolve();
+      }
+
+      return propertyConverterInstance.getValues().then(function(values) {
+
+        /**
+         * Sticky Sidebar instance.
+         *
+         * @type {StickySidebar}
+         */
+        stickySidebarsInstance = new StickySidebar(
+          $element[0],
+          $.extend({}, stickySettings, {
+            bottomSpacing:  values[bottomOffsetPropertyName],
+            topSpacing:     values[topOffsetPropertyName]
+          })
+        );
+
+      });
+
+    };
+
+    this.disable = function() {
+
+      if (typeof stickySidebarsInstance === 'undefined') {
+        return Promise.resolve();
+      }
+
+      return fastdom.mutate(function() {
+
+        stickySidebarsInstance.destroy();
+
+        stickySidebarsInstance = undefined;
+
+      });
+
+    };
+
+    this.destroy = function() {
+
+      $element.off(
+        'responsivePropertyChange.' + eventNamespace, responsivePropertyHandler
+      );
+
+      return this.disable();
+
+    };
+
+  };
+
   /**
    * Build a Sticky Sidebar instance for the provided sidebars container.
    *
@@ -99,41 +217,41 @@ function(sidebarsSticky, $) {
    *   initialized. If one is already active, this will be an already resolved
    *   Promise object.
    */
-  function build($container) {
+  // function build($container) {
 
-    if (typeof $container.prop('stickySidebar') !== 'undefined') {
-      return Promise.resolve();
-    }
+    // if (typeof $container.prop('stickySidebar') !== 'undefined') {
+    //   return Promise.resolve();
+    // }
 
-    return $container.prop(propertyConverterInstanceName).getValues()
-    .then(function(values) {
+    // return $container.prop(propertyConverterInstanceName).getValues()
+    // .then(function(values) {
 
       /**
        * Second sidebar.
        *
        * @type {HTMLElement}
        */
-      let sidebarSecond = $container.find('.layout-sidebar-second')[0];
+    //   let sidebarSecond = $container.find('.layout-sidebar-second')[0];
 
       /**
        * Sticky Sidebar instance.
        *
        * @type {StickySidebar}
        */
-      let stickySidebar = new StickySidebar(sidebarSecond, {
-        innerWrapperSelector: '.region-sidebar-second',
-        containerSelector:    'main',
-        bottomSpacing:        values[bottomOffsetPropertyName],
-        topSpacing:           values[topOffsetPropertyName]
-      });
+  //     let stickySidebar = new StickySidebar(sidebarSecond, {
+  //       innerWrapperSelector: '.region-sidebar-second',
+  //       containerSelector:    'main',
+  //       bottomSpacing:        values[bottomOffsetPropertyName],
+  //       topSpacing:           values[topOffsetPropertyName]
+  //     });
 
-      $container.addClass(containerEnhancedStickyClass);
+  //     $container.addClass(containerEnhancedStickyClass);
 
-      $container.prop('stickySidebar', stickySidebar);
+  //     $container.prop('stickySidebar', stickySidebar);
 
-    });
+  //   });
 
-  };
+  // };
 
   /**
    * Destroy a Sticky Sidebar instance for the provided sidebars container.
@@ -147,106 +265,154 @@ function(sidebarsSticky, $) {
    *   destroyed. If one isn't active, this will be an already resolved Promise
    *   object.
    */
-  function destroy($container) {
+  // function destroy($container) {
 
-    if (typeof $container.prop('stickySidebar') === 'undefined') {
-      return Promise.resolve();
-    }
+  //   if (typeof $container.prop('stickySidebar') === 'undefined') {
+  //     return Promise.resolve();
+  //   }
 
-    return fastdom.mutate(function() {
+  //   return fastdom.mutate(function() {
 
-      $container.prop('stickySidebar').destroy();
+  //     $container.prop('stickySidebar').destroy();
 
-      $container
-      .removeProp('stickySidebar')
-      .removeClass(containerEnhancedStickyClass);
+  //     $container
+  //     .removeProp('stickySidebar')
+  //     .removeClass(containerEnhancedStickyClass);
 
-    });
+  //   });
 
-  };
+  // };
 
   this.addBehaviour(
-    'OmnipediaSiteThemeSidebarsSticky',
-    'omnipedia-site-theme-sidebars-sticky',
-    sidebarsElements.getSidebarsBehaviourSelector(),
+    'OmnipediaSiteThemeSidebarSecondSticky',
+    'omnipedia-site-theme-sidebar-second-sticky',
+    '.region-sidebar-second',
     function(context, settings) {
+
+      let $element = $(this);
+
+      let instance = new stickyElement(
+        $element.closest('.layout-sidebar-second'),
+        {
+          innerWrapperSelector: '.region-sidebar-second',
+          containerSelector:    'main',
+        }
+      );
+
+      $element.prop(stickyElementInstancePropertyName, instance);
+
+      $element.closest('.layout-container').addClass(
+        containerEnhancedStickyClass
+      );
+
+      // @todo Instance manager.
+      activeInstances.sidebarSecond = instance;
+
+    },
+    function(context, settings, trigger) {
+
+      delete activeInstances.sidebarSecond;
+
+      let $element = $(this);
+
+      $element.prop(stickyElementInstancePropertyName).destroy();
+
+      $element.removeProp(stickyElementInstancePropertyName);
+
+      // @todo Instance manager.
+      if (Object.values(activeInstances).length === 0) {
+
+        $element.closest('.layout-container').removeClass(
+          containerEnhancedStickyClass
+        );
+
+      }
+
+    }
+  );
+
+  // this.addBehaviour(
+  //   'OmnipediaSiteThemeSidebarsSticky',
+  //   'omnipedia-site-theme-sidebars-sticky',
+  //   sidebarsElements.getSidebarsBehaviourSelector(),
+  //   function(context, settings) {
 
       /**
        * The sidebars container jQuery collection.
        *
        * @type {jQuery}
        */
-      let $sidebarsContainer = sidebarsElements.getSidebarsContainer();
+      // let $sidebarsContainer = sidebarsElements.getSidebarsContainer();
 
-      $sidebarsContainer
-      .on('responsivePropertyChange.' + eventNamespace, function(
-        event, instance
-      ) {
+      // $sidebarsContainer
+      // .on('responsivePropertyChange.' + eventNamespace, function(
+      //   event, instance
+      // ) {
 
-        // Ignore any other responsive properties that may be watched on this
-        // or descendent elements.
-        if (instance.getPropertyName() !== isStickyPropertyName) {
-          return;
-        }
+      //   // Ignore any other responsive properties that may be watched on this
+      //   // or descendent elements.
+      //   if (instance.getPropertyName() !== isStickyPropertyName) {
+      //     return;
+      //   }
 
-        // Attempt to build an instance if one is not already active.
-        if (instance.getValue() === 'true') {
-          build($sidebarsContainer);
+      //   // Attempt to build an instance if one is not already active.
+      //   if (instance.getValue() === 'true') {
+      //     build($sidebarsContainer);
 
-        // Attempt to destroy an instance if one is active.
-        } else if (instance.getValue() === 'false') {
-          destroy($sidebarsContainer);
-        }
+      //   // Attempt to destroy an instance if one is active.
+      //   } else if (instance.getValue() === 'false') {
+      //     destroy($sidebarsContainer);
+      //   }
 
-      });
+      // });
 
       /**
        * Property to pixel converter instance.
        *
        * @type {converter}
        */
-      let propertyConverter = aiPropertyToPixelConverter.create(
-        $sidebarsContainer, [bottomOffsetPropertyName, topOffsetPropertyName]
-      );
+      // let propertyConverter = aiPropertyToPixelConverter.create(
+      //   $sidebarsContainer, [bottomOffsetPropertyName, topOffsetPropertyName]
+      // );
 
-      $sidebarsContainer.prop(
-        propertyConverterInstanceName, propertyConverter
-      );
+      // $sidebarsContainer.prop(
+      //   propertyConverterInstanceName, propertyConverter
+      // );
 
       /**
        * A responsive style property instance; watches sticky state.
        *
        * @type {responsiveStyleProperty}
        */
-      let responsiveStyleProperty = aiResponsiveStyleProperty.create(
-        isStickyPropertyName, $sidebarsContainer
-      );
+      // let responsiveStyleProperty = aiResponsiveStyleProperty.create(
+      //   isStickyPropertyName, $sidebarsContainer
+      // );
 
-      // Save the responsiveStyleProperty to the container for detach.
-      $sidebarsContainer.prop(
-        responsivePropertyInstanceName, responsiveStyleProperty
-      );
+      // // Save the responsiveStyleProperty to the container for detach.
+      // $sidebarsContainer.prop(
+      //   responsivePropertyInstanceName, responsiveStyleProperty
+      // );
 
-    },
-    function(context, settings, trigger) {
+    // },
+    // function(context, settings, trigger) {
 
       /**
        * The sidebars container jQuery collection.
        *
        * @type {jQuery}
        */
-      let $sidebarsContainer = sidebarsElements.getSidebarsContainer();
+      // let $sidebarsContainer = sidebarsElements.getSidebarsContainer();
 
-      $sidebarsContainer.off('responsivePropertyChange.' + eventNamespace);
+      // $sidebarsContainer.off('responsivePropertyChange.' + eventNamespace);
 
-      $sidebarsContainer.prop(responsivePropertyInstanceName).destroy();
+      // $sidebarsContainer.prop(responsivePropertyInstanceName).destroy();
 
-      $sidebarsContainer
-      .removeProp(responsivePropertyInstanceName)
-      .removeProp(propertyConverterInstanceName);
+      // $sidebarsContainer
+      // .removeProp(responsivePropertyInstanceName)
+      // .removeProp(propertyConverterInstanceName);
 
-    }
-  );
+  //   }
+  // );
 
 });
 });
