0db6c896e63d94dac90aef09ecc8b504288a54c8
Matei Stanca <i@ambientimpact.com>
2024-01-22 08:05:41 -0500

    offcanvas.history: Now preserves existing history state when replacing.

--- javascript/offcanvas.history.js
+++ javascript/offcanvas.history.js
@@ -37,7 +37,23 @@ AmbientImpact.addComponent('OmnipediaSiteThemeOffcanvasHistory', function(
 
         $(window).one(`popstate.${eventNamespace}`, function(event) {
 
-          history.replaceState({offcanvasPanel: 'popped'}, document.title);
+          /**
+           * Copy of the history state we're popping to.
+           *
+           * Since we're using history.replaceState(), we need to make sure we
+           * don't accidentally delete any existing history state data that may
+           * have been added by something else. Deleting existing data can be
+           * especially bad when using something like Hotwire Turbo, in which
+           * case we would end up with back and forward navigation only changing
+           * the URL but no page changes.
+           *
+           * @type {Object}
+           */
+          let state = event.originalEvent.state;
+
+          state.offcanvasPanel = 'popped';
+
+          history.replaceState(state, document.title);
 
           // There's no API to open or close the panel yet, so we have to
           // trigger a click on the close button.
