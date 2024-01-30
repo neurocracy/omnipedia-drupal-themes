94935f73c97902bd258d4d81c0bacb513c141174
Matei Stanca <i@ambientimpact.com>
2024-01-30 15:39:49 -0500

    Issue #14: Logo shimmer now plays on leaving immerse mode as well.

--- javascript/site_branding.js
+++ javascript/site_branding.js
@@ -28,6 +28,20 @@ AmbientImpact.addComponent('OmnipediaSiteThemeSiteBranding', function(
   const eventNamespace = this.getName();
 
   /**
+   * The selector to find the header element by.
+   *
+   * @type {String}
+   */
+  const headerSelector = 'header[role="banner"]';
+
+  /**
+   * Class applied to the header when immerse has been entered at least once.
+   *
+   * @type {String}
+   */
+  const immersedOnceClass = 'header-immersed-once';
+
+  /**
    * Class applied to the header element when it's been unpinned at least once.
    *
    * @type {String}
@@ -35,9 +49,9 @@ AmbientImpact.addComponent('OmnipediaSiteThemeSiteBranding', function(
   const unpinnedOnceClass = 'headroom--unpinned-once';
 
   this.addBehaviour(
-    'OmnipediaSiteThemeSiteBranding',
-    'omnipedia-site-theme-site-branding',
-    'header[role="banner"]',
+    'OmnipediaSiteThemeSiteBrandingUnpinOnce',
+    'omnipedia-site-theme-site-branding-unpin-once',
+    headerSelector,
     function(context, settings) {
 
       $(this).on(`headroomUnpin.${eventNamespace}`, function(event) {
@@ -50,6 +64,26 @@ AmbientImpact.addComponent('OmnipediaSiteThemeSiteBranding', function(
       $(this).off(`headroomUnpin.${eventNamespace}`).removeClass(
         unpinnedOnceClass,
       );
+
+    }
+  );
+
+  this.addBehaviour(
+    'OmnipediaSiteThemeSiteBrandingImmersedOnce',
+    'omnipedia-site-theme-site-branding-immersed-once',
+    'body',
+    function(context, settings) {
+
+      $(this).on(`immerseEnter.${eventNamespace}`, function(event) {
+        $(this).find(headerSelector).addClass(immersedOnceClass);
+      });
+
+    },
+    function(context, settings, trigger) {
+
+      $(this).off(`immerseEnter.${eventNamespace}`).find(
+        headerSelector,
+      ).removeClass(immersedOnceClass);
 
     }
   );
