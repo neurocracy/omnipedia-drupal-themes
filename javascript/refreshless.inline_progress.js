0d40ecc8f87b2ccb5ad4086cf89d03619df831b1
Matei Stanca <i@ambientimpact.com>
2024-11-29 11:25:36 -0500

    Issue #27: Adapted inline progress to new RefreshLess progress bar.

--- javascript/refreshless.inline_progress.js
+++ javascript/refreshless.inline_progress.js
@@ -2,7 +2,6 @@
 //   Omnipedia - Site theme - RefreshLess inline progress
 // -----------------------------------------------------------------------------
 
-AmbientImpact.onGlobals(['Turbo.config.drive.progressBarDelay'], () => {
 AmbientImpact.on(['fastdom'], (aiFastDom) => {
 AmbientImpact.addComponent(
   'OmnipediaSiteThemeRefreshLessInlineProgress',
@@ -24,12 +23,25 @@ AmbientImpact.addComponent(
    */
   const fastdom = aiFastDom.getInstance();
 
-  const progressDelayCustomProp = '--refreshless-progress-bar-delay';
-
+  /**
+   * Class added to links when clicked and a progress bar will be displayed.
+   *
+   * @type {String}
+   */
   const progressClass = 'refreshless-inline-progress-bar';
 
+  /**
+   * Modifier class added to links when their progress bar is active/visible.
+   *
+   * @type {String}
+   */
   const progressActiveClass = `${progressClass}--active`;
 
+  /**
+   * Modifier class added to links when their progress bar is complete.
+   *
+   * @type {String}
+   */
   const progressCompleteClass = `${progressClass}--complete`;
 
   component.addBehaviour(
@@ -40,24 +52,51 @@ AmbientImpact.addComponent(
 
       const $html = $('html');
 
-      $html
-      .css(progressDelayCustomProp, `${Turbo.config.drive.progressBarDelay}ms`)
-      .on(`turbo:click.${eventNamespace}`, async (event) => {
+      $html.on(`turbo:click.${eventNamespace}`, async (event) => {
 
         const $link = $(event.target);
 
-        await fastdom.mutate(() => {
-          $link.addClass([progressClass, progressActiveClass]);
-        });
-
         $html
-        .one(`refreshless:before-render.${eventNamespace}`, async (event) => {
+        .one(`refreshless:progress-bar-before-show.${eventNamespace}`, async (
+          event,
+        ) => {
+
+          await fastdom.mutate(() => {
+            $link.addClass([progressClass, progressActiveClass]);
+          });
+
+        })
+        .one(`refreshless:progress-bar-before-hide.${eventNamespace}`, async (
+          event,
+        ) => {
+
+          await fastdom.mutate(() => {
+            $link.removeClass([
+              progressClass, progressActiveClass, progressCompleteClass,
+            ]);
+          });
+
+        })
+        .one(`refreshless:before-render.${eventNamespace}`, async (
+          event,
+        ) => {
 
           await fastdom.mutate(() => {
             $link
             .addClass(progressCompleteClass)
             .removeClass(progressActiveClass);
           });
+
+        })
+        // This removes the progress bar event handlers in case they weren't
+        // triggered after a click, i.e. if the page loaded too quickly for the
+        // progress bar to be displayed at all.
+        .one(`refreshless:attach.${eventNamespace}`, async (event) => {
+
+          $html.off([
+            `refreshless:progress-bar-before-show.${eventNamespace}`,
+            `refreshless:progress-bar-before-hide.${eventNamespace}`,
+          ].join(' '));
 
         });
 
@@ -66,11 +105,13 @@ AmbientImpact.addComponent(
     },
     (context, settings, trigger) => {
 
+      // We're only removing the click event handler because if it was
+      // triggered, it will have attached a one-off refreshless:attach event
+      // handler to remove the other handlers if they're bound.
       $('html').off(`turbo:click.${eventNamespace}`);
 
     }
   );
 
-});
 });
 });
